b5a6c84025ee360db36f60e78d7a7f0e4b9135b8
Alina3143 <117859339+Alina3143@users.noreply.github.com>
2022-11-16 13:23:39 -0700

    Update second_steam_app.py

--- second_steam_app.py
+++ second_steam_app.py
@@ -21,3 +21,4 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 fruityjuice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.text(fruityvice_response)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
