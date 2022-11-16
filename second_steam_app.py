8dfdb9a7cdf40712d76e406825970ea333c5ba05
Alina3143 <117859339+Alina3143@users.noreply.github.com>
2022-11-16 12:11:52 -0700

    Update second_steam_app.py

--- second_steam_app.py
+++ second_steam_app.py
@@ -1,5 +1,7 @@
 import streamlit
 import pandas
+import requests
+
 streamlit.title("SF Badge Ass 1")
 streamlit.header('Breakfast Menu')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
@@ -12,3 +14,5 @@ fruits_selected = streamlit.multiselect("Pick some fruits", list(my_fruit_list.i
 #streamlit.dataframe(my_fruit_list)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
