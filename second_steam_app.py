5fe8078fb48314593c2989828465564ad8fed35d
Alina3143 <117859339+Alina3143@users.noreply.github.com>
2022-11-18 09:28:33 -0700

    Update second_steam_app.py

--- second_steam_app.py
+++ second_steam_app.py
@@ -26,7 +26,6 @@ streamlit.dataframe(fruityvice_normalized)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-
 fruityjuice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.text(fruityjuice_response)
 fruityjuice_normalized = pandas.json_normalize(fruityjuice_response.json())
@@ -46,9 +45,7 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("Fruits to choose from:")
 streamlit.dataframe(my_data_row)
-#streamlit.write('The user entered ', fruit_choice)
 
-#fruityjuice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 add_my_fruit =  streamlit.text_input('What fruit would you like information to ADD ?','Melon')
 streamlit.text(add_my_fruit)
 #my_cur.execute("INSERT INTO  fruit_load_list (FRUIT_NAME) VALUES(" +add_my_fruit+ ')')
