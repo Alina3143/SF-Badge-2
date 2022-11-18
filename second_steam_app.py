137e69ed756a6388ac0830392034d62adecb03da
Alina3143 <117859339+Alina3143@users.noreply.github.com>
2022-11-18 09:51:42 -0700

    Update second_steam_app.py

--- second_steam_app.py
+++ second_steam_app.py
@@ -20,14 +20,15 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.header("Fruityvice Fruit Advice!")
+
+streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else   
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        streamlit.text(fruityvice_response)
+        #streamlit.text(fruityvice_response)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         streamlit.dataframe(fruityvice_normalized)
         
